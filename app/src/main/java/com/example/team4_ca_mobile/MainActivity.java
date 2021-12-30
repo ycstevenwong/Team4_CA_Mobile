1c481cf70139ad7f1f459e3263f59fbde56964da
Steven Wong <ycstevenwong@gmail.com>
2021-12-30 21:15:27 +0800

    add login and sign up button

--- app/src/main/java/com/example/team4_ca_mobile/MainActivity.java
+++ app/src/main/java/com/example/team4_ca_mobile/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.team4_ca_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginOrSignUpBtn();
+    }
+
+    protected void loginOrSignUpBtn(){
+        int[] ids = {R.id.loginButton,R.id.signUpButton};
+        for(int i = 0; i < ids.length;i++){
+            Button btn = findViewById(ids[i]);
+            if(btn!=null){
+                btn.setOnClickListener(this);
+            }
+        }
+    }
+    @Override
+    public void onClick(View v){
+        int id = v.getId();
+        if(id == R.id.loginButton){
+            Intent intent = new Intent(this,login.class);
+            startActivity(intent);
+        }
+        if(id == R.id.signUpButton){
+            Intent intent = new Intent(this,sign_up.class);
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/example/team4_ca_mobile/sign_up.java
@@ -0,0 +1,14 @@
+package com.example.team4_ca_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sign_up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
